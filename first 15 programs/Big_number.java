d128e716493f288aa0d758c9832a738a5b6fe2d1
mparu <mparundhathi@gmail.com>
2021-05-26 12:56:12 +0530

    java

---
+++ first 15 programs/Big_number.java
@@ -0,0 +1,19 @@
+public class Big_number
+{
+public static void main(String args[])
+{
+int a=5,b=6,c=2;
+if(a>=b&&a>=c)
+System.out.println("the biggest number is="+a);
+else if(b>=a&&b>=c)
+System.out.println("the biggest number is="+b);
+else
+System.out.println("the biggest number is="+c);
+}
+}
+
+
+
+
+
+
---
+++ first 15 programs/Circle.java
@@ -0,0 +1,10 @@
+public class Circle{
+public Static void main(String args[]){
+double r=7.5f;
+double area,peri;
+area=3.14*r;
+peri=2*3.14*r;
+System.out.println("The area of circle is="+area);
+System.out.println("The perimeter of circle is="+peri);
+}
+}
