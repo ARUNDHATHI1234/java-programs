11cf53d462d5d9efe81e2eefec509d9e8f45ee58
mparu <mparundhathi@gmail.com>
2021-05-26 13:36:24 +0530

    java

---
+++ first 15 programs/Sum_Avg.java
@@ -0,0 +1,13 @@
+public class Sum_Avg{
+public static void main(String args[])
+{
+int a=2,b=3,c=4,d=5,e=6,sum=0,avg=0;
+sum=a+b+c+d+e;
+avg=sum/5;
+System.out.println("the sum of five numbers="+sum);
+System.out.println("the average of five numbers="+avg);
+}
+}
+
+
+
