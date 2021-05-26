9cf4541c4c3cf78fd3a1b447842d7ad46c158d0c
mparu <mparundhathi@gmail.com>
2021-05-26 14:32:03 +0530

    java'

---
+++ first 15 programs/Array.java
@@ -0,0 +1,9 @@
+public class Array{
+public static void main(String Rgs[])
+{
+int[] num={12,4,5,2};
+System.out.println("the first element:"+num[0]);
+System.out.println("the second element:"+num[1]);
+System.out.println("the third element:"+num[2]);
+}
+}
