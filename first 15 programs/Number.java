57345b9cff0d317a0949612848348fb77ac88cc0
mparu <mparundhathi@gmail.com>
2021-05-26 12:02:22 +0530

    java

---
+++ first 15 programs/Number.java
@@ -0,0 +1,15 @@
+public class Number
+{
+public static void main(String[] args) 
+{
+int num=100;
+if(num>0)
+{
+System.out.println(num+" is a positive number");
+}
+else if(num<0)
+{
+System.out.println(num+" is a negative number");
+}
+}
+}
