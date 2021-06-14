2fc166e64a1b136bae297b0b1a36c647aeca2ef6
mparu <mparundhathi@gmail.com>
2021-06-14 20:23:47 +0530

    'java

--- Userinput/ArrayLargest.java
+++ Userinput/ArrayLargest.java
@@ -7,7 +7,7 @@ int n;
 Scanner sc= new Scanner(System.in);
 System.out.println("Enter number of elements in the array:");
 n=sc.nextInt();
-int a[] = new int[n];
+int[]  a=new int[n];
 System.out.println("Enter elements of array:");
 for(int i = 0;i<n;i++)
 {
@@ -16,11 +16,11 @@ a[i]=sc.nextInt();
 int large=a[0];
 for(int i = 0;i<n;i++)
 {
-if(max<a[i])
+if(large<a[i])
 {
 large=a[i];
 }
 }
-System.out.println("the largest element is:"+max);
+System.out.println("the largest element is:"+large);
 }
 }
